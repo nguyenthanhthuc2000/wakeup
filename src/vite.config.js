0071f33d9bfc43e09a5d54670a1612828bc2dcbe
Thức Nguyễn <nguyenthanhthuc.2k@gmail.com>
2025-03-24 06:54:33 +0700

    wakeup-ssr | init template

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     server: {
@@ -12,9 +11,19 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/scss/style.scss',
+                'resources/scss/icons.scss',
+                'resources/js/pages/dashboard.init.js',
+                'resources/plugins/morris/morris.min.js', // Morris Chart CSS
+                'resources/plugins/raphael/raphael.min.js', // Morris Chart CSS
+                'resources/js/jquery.min.js',
+                'resources/js/bootstrap.bundle.min.js',
+                'resources/js/jquery.slimscroll.js',
+                'resources/js/waves.min.js',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
-        tailwindcss(),
     ],
 });
